76d9832b01c497b5171689073e0f937e2251f593
J. Paterson <contact@jpatersondev.com>
2019-12-14 10:11:03 -0800

    create basic posts gql query and migration

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -2,6 +2,6 @@ module Types
   class PostType < Types::BaseObject
     field :title, String, null: true
     field :rating, Integer, null: true
-    field :comments, [Types::CommentType], null: true
+    # field :comments, [Types::CommentType], null: true
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,14 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :post, PostType, null: false
+    def post
+      Post.first
     end
+    field :posts, [PostType], null: false
+    def posts
+      Post.all
+    end
+    
   end
 end
